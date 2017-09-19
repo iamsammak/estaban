653ca0290336ac7838dbf07d123494ddbc74f0d6
Samuel Mak <samuelswmak@gmail.com>
2017-09-19 01:04:21 -0700

    add weather components

---
+++ js/components/root.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Weather from './weather';
+
+const Root = ({ store }) => {
+  return(
+    <div>
+      <Weather />
+    </div>
+  );
+};
+
+export default Root;
---
+++ js/components/weather.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import {
+  cloudy, rainy,
+  cloudy_with_sun, cloudy_with_lightning, cloudy_with_moon,
+  cloudy_with_rain_and_lightning, sunny, sunny_with_wind,
+  clear_night, snowy } from './weather_icons';
+
+class Weather extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weather: null
+    };
+  }
+
+  render() {
+
+    return(
+      <div className="container">
+        <div className="elements">
+          {cloudy}
+          {rainy}
+          {cloudy_with_sun}
+          {cloudy_with_lightning}
+          {cloudy_with_moon}
+          {cloudy_with_rain_and_lightning}
+          {sunny}
+          {clear_night}
+          {sunny_with_wind}
+          {snowy}
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Weather;
